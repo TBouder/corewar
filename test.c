aff8baf0e8eb60083929f67dd149d14fe5ce7887
Tbouder <tbouder@protonmail.com>
2016-11-09 19:06:35 +0100

    asm

---
+++ test.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/09 18:17:24 by tbouder           #+#    #+#             */
+/*   Updated: 2016/11/09 19:06:05 by tbouder          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+
+int		main(int ac, char **av)
+{
+	int a;
+    int b = 5;
+	char *one;
+	char *two = ft_strinit(av[1]);
+	//	//movl %edx, %eax means Move the contents of the edx register into the eax register
+    // asm
+	// (
+	// 	"movl %1, %0"
+	// 	: "=&r" (a)
+	// 	: "r" (b)
+	// 	// : /* liste des modifications */
+	// );
+	// ft_printf("a = %d && b = %d\n", a, b);
+
+    asm
+	(
+		// "str %1, %0"
+		// : "=&r" (one)
+		// : "r" (two)
+		// : /* liste des modifications */
+	);
+
+	ft_printf("a = %s && b = %s\n", one, two);
+    return 0;
+}
