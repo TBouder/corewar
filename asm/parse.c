430aa148dc43c2f5c76521d4c0314a581d845d17
Quentin Roulon <quroulon@student.42.fr>
2016-11-15 18:38:18 +0100

    upd : recup .name, .comment

--- asm/parse.c
+++ asm/parse.c
@@ -6,7 +6,7 @@
 /*   By: quroulon <quroulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 17:46:27 by quroulon          #+#    #+#             */
-/*   Updated: 2016/11/15 17:22:02 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/11/15 18:33:45 by quroulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,21 @@ void		ft_recover_champ_infos(t_asm *env, int i)
 	char	*tmp;
 
 	tmp = ft_strsub(env->file_content[i], 0, 5);
+	//TODO replace .name by NAME_CMD_STRING
 	if (EQU(tmp, ".name") == 1 && !env->champ_name)
 		ft_recover_name(env, i);
 	else
 	{
 		ft_strdel(&tmp);
 		tmp = ft_strsub(env->file_content[i], 0, 8);
+		//TODO replace .comment by COMMENT_CMD_STRING
 		if (EQU(tmp, ".comment") == 1 && !env->champ_comment)
 			ft_recover_description(env, i);
+		else
+		{
+			ft_strdel(&tmp);
+			ft_error_asm(env, BAD_SRC_FILE, 1);
+		}
 	}
 	ft_strdel(&tmp);
 }
