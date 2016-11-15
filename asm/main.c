b4f72176e31d70559ddc4f12a8761dd0736b28b2
Tbouder <tbouder@protonmail.com>
2016-11-15 14:41:06 +0100

    Add the db functions in the libft and split the main file

--- asm/main.c
+++ asm/main.c
@@ -6,112 +6,12 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 16:03:50 by tbouder           #+#    #+#             */
-/*   Updated: 2016/11/15 14:08:55 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/11/15 14:38:24 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-/*********************************ASM******************************************/
-int		ft_strlen_asm(char *str)
-{
-	char	*d;
-	int		len;
-
-	d = NULL;
-	__asm__ __volatile__
-	(
-		"SUB	%%ECX, %%ECX\n"
-		"SUB	%%AL, %%AL\n"
-		"NOT	%%ECX\n"
-		"CLD	\n"
-		"REPNE	SCASB\n"
-		"NOT	%%ECX\n"
-		"DEC	%%ECX\n"
-		"MOVL	%%ECX, %1\n"
-		: "=&D" (d), "=&r" (len)
-		: "0" (str)
-		: "memory"
-	);
-	return (len);
-}
-
-char	*ft_strinit_asm(char *str)
-{
-	char	*dest;
-	int		s;
-	int		d;
-	int		a;
-
-	s = 0;
-	d = 0;
-	a = 0;
-	dest = ft_strnew(ft_strlen(str));
-	__asm__ __volatile__
-	(
-		"CLD		\n"
-		"loop:"
-			"LODSB	\n"
-			"STOSB	\n"
-			"TESTB	%%AL, %%AL\n"
-			"JNE	loop"
-		: "=&S" (s), "=&D" (d), "=&a" (a)
-		: "0" (str), "1" (dest), "2" (0)
-		: "memory"
-	);
-	return (dest);
-}
-/********************************!ASM******************************************/
-
-/********************************LIBFT*****************************************/
-
-char	**ft_dbstrnew(size_t size)
-{
-	char		**buffer;
-	size_t		i;
-
-	buffer = NULL;
-	if (!(buffer = (char **)malloc(sizeof(char *) * (size + 1))))
-		return (NULL);
-	i = 0;
-	while (i < size + 1)
-	{
-		buffer[i] = NULL;
-		i++;
-	}
-	return (buffer);
-}
-
-void	ft_dbstrassign(char **tab, t_list *lst, size_t size)
-{
-	size_t		i;
-
-	i = 0;
-	if (!tab)
-		return ;
-	while (i < size && lst)
-	{
-		tab[i] = ft_strinit((char *)lst->content);
-		i++;
-		lst = lst->next;
-	}
-}
-
-void	ft_putdbstr(char **tab, size_t size)
-{
-	size_t		i;
-
-	i = 0;
-	if (!tab)
-		return ;
-	while (i < size)
-	{
-		ft_printf("[{10}%s{0}]\n", tab[i]);
-		i++;
-	}
-}
-
-/*******************************!LIBFT*****************************************/
 
 void	ft_clear_all(t_asm *env)
 {
@@ -141,7 +41,7 @@ void	ft_get_file_content(t_asm *env)
 	lst = NULL;
 	while (get_next_line(env->fd, &line))
 	{
-		ft_lstend(&lst, (char *)line, ft_strlen(line) + 1);
+		ft_lstend(&lst, (char *)line, ft_strlen_asm(line) + 1);
 		env->file_len++;
 		ft_strdel(&line);
 	}
@@ -173,12 +73,12 @@ char	ft_verif_extension(t_asm *env, char *source)
 	errno = 0;
 	if (source == NULL || (env->fd = open(source, O_RDONLY)) == -1 || errno != 0)
 		ft_error_asm(source);
-	extension = ft_strinit(ft_strrchr(source, '.'));
+	extension = ft_strinit_asm(ft_strrchr(source, '.'));
 	result = EQU(extension, ".s");
 	if (result)
 	{
-		len_source = ft_strlen(source);
-		env->filename = ft_strinit(source);
+		len_source = ft_strlen_asm(source);
+		env->filename = ft_strinit_asm(source);
 		env->filename_noext = ft_strsub(source, 0, len_source - 2);
 	}
 	ft_strdel(&extension);
