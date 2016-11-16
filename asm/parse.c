301c938bbd9ccb85f98ad29bb108f34a8f5bca9d
Tbouder <tbouder@protonmail.com>
2016-11-16 14:30:09 +0100

    Free

--- asm/parse.c
+++ asm/parse.c
@@ -6,7 +6,7 @@
 /*   By: quroulon <quroulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 17:46:27 by quroulon          #+#    #+#             */
-/*   Updated: 2016/11/16 14:20:05 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/11/16 14:29:59 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ void		ft_recover_description(t_asm *env, int i)
 	env->champ_comment = ft_strtrim_char(tmp, '"');
 	if (ft_strlen(env->champ_comment) - 1 > COMMENT_LENGTH)
 		ft_error_asm(env, CHAMP_COMMENT_TOO_LONG, 1);
-	tmp = NULL;
 }
 
 void		ft_recover_name(t_asm *env, int i)
@@ -31,7 +30,6 @@ void		ft_recover_name(t_asm *env, int i)
 	env->champ_name = ft_strtrim_char(tmp, '"');
 	if (ft_strlen(env->champ_name) - 1 > PROG_NAME_LENGTH)
 		ft_error_asm(env, CHAMP_NAME_TOO_LONG, 1);
-	tmp = NULL;
 }
 
 void		ft_recover_champ_infos(t_asm *env, int i)
@@ -40,20 +38,25 @@ void		ft_recover_champ_infos(t_asm *env, int i)
 
 	tmp = ft_strsub(env->file_content[i], 0, 5);
 	if (EQU(tmp, NAME_CMD_STRING) == 1 && !env->champ_name)
+	{
 		ft_recover_name(env, i);
+		ft_strdel(&tmp);
+	}
 	else
 	{
 		ft_strdel(&tmp);
 		tmp = ft_strsub(env->file_content[i], 0, 8);
 		if (EQU(tmp, COMMENT_CMD_STRING) == 1 && !env->champ_comment)
+		{
+			ft_strdel(&tmp);
 			ft_recover_description(env, i);
+		}
 		else
 		{
 			ft_strdel(&tmp);
 			ft_error_asm(env, BAD_SRC_FILE, 1);
 		}
 	}
-	ft_strdel(&tmp);
 }
 
 // void		ft_get_command(t_asm *env, int i)
