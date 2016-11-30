bb87a6a604e589c3ff199fe1f44ccc5e726af320
Quentin Roulon <quroulon@student.42.fr>
2016-11-30 18:58:49 +0100

    upd : write instruction work except to [ld %-15  r3] in Death.s

--- asm/ft_write_instructions.c
+++ asm/ft_write_instructions.c
@@ -6,87 +6,117 @@
 /*   By: quroulon <quroulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 16:31:46 by quroulon          #+#    #+#             */
-/*   Updated: 2016/11/30 13:37:51 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/11/30 18:52:21 by quroulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-static int		ft_jump_to_label(t_asm *env, char *str, int line, int arg)
+long		ft_atoi_l(const char *str)
 {
-	int			i;
-	int			j;
-	int			found;
-	int			cpt;
-	char		**tab;
+	long	result;
+	int		sign;
 
-	i = 0;
-	cpt = 0;
-	found = 0;
-	arg = 0;
-	str = ft_strjoin(str, ":");
-	while (env->file_content[i])
+	sign = 1;
+	result = 0;
+	if (ft_strlen(str) == 0 || (*str < 32 && !ft_isspace(*str)))
+		return (0);
+	while (*str <= 32)
+		str++;
+	if ((*str == '-' || *str == '+'))
 	{
-		tab = ft_strsplit(env->file_content[i], ' ');
-		if (ft_isstrstr(tab[0], str) == 1)
-		{
-			if (found != 0)
-			{
-				ft_strdel(&str);
-				ft_dbstrdel(tab);
-				return (cpt * found);
-			}
-			found = -1;
-		}
-		j = 0;
-		if (line == i)// && found == 0)
-		{
-			if (found < 0)
-			{
-				ft_strdel(&str);
-				ft_dbstrdel(tab);
-				return ((USHRT_MAX + 1) - cpt);
-			}
-			found = 1;
-		}
-		while (j <= 3 && env->arg_weight[i][j] > 0)
-		{
-			cpt += (found != 0) ? env->arg_weight[i][j] : 0;
-			j++;
-		}
-		ft_dbstrdel(tab);
-		i++;
+		if (*str == '-')
+			sign = -1;
+		str++;
 	}
-	ft_strdel(&str);
+	while (*str >= '0' && *str <= '9')
+		result = (result + *str++ - '0') * 10;
+	return (result / 10 * sign);
+}
 
-	return (0);
+
+long				ft_calcul_jump(t_asm *env, char *str, int line, int pds)
+{
+	long		cpt;
+	int			match;
+	char		*label;
+
+	cpt = 0;
+	match = -1;
+	label = ft_strjoin(str, ":");
+	ft_btreesearch_asm(env->file_labels, label, &match);
+	if (match > 0 && line < match)
+	{
+		while (line < match)
+		{
+			cpt += env->instruct_weight[line];
+			line++;
+		}
+	}
+	else if (match > 0 && line > match)
+	{
+		line--;
+		while (line > match)
+		{
+			cpt += env->instruct_weight[line];
+			line--;
+		}
+		if (pds == 2)
+			cpt = (cpt != 0) ? (USHRT_MAX + 1) - cpt : 0;
+		else if (pds == 4)
+			cpt = (cpt != 0) ? (UINT_MAX + 1) - cpt : 0;
+	}
+	return (cpt);
 }
 
 static void		ft_write_args(t_asm *env, char **args, int *pds, int line)
 {
 	int			i;
+	long		tmp;
 
 	i = 1;
+	tmp = 0;
 	while (i <= 3 && pds[i] > 0)
 	{
 		if (args[i][0] && args[i][0] == 'r')
-			ft_transform_size(ft_atoi_hexa(&args[i][1]), env->fd, pds[i]);
+			ft_transform_size(ft_atoi(&args[i][1]), env->fd_cor, pds[i]);
 		else if (args[i][0] && args[i][0] == '%')
 		{
 			if (args[i][1] && args[i][1] == ':')
-				ft_transform_size(ft_jump_to_label(env, &args[i][2], line, i),
-								env->fd, pds[i]);
+				ft_transform_size(ft_calcul_jump(env, &args[i][2], line, pds[i]), env->fd_cor, pds[i]);
 			else
 			{
 				//ATTENTION, DE TEMPS EN TEMPS, ON SE RETROUVE AVEC, DANS TRANSFORME SIZE, [content < 0] (donc pds[i] < 0)
 				// J'ai ajoute une condition pour bloquer et eviter les leaks et invalid rights si c'est le cas, mais faudra
 				// surveiller
-				ft_printf("[%s : %d]\n", args[i], ft_atoi_hexa(&args[i][1]));
-				ft_transform_size(ft_atoi_hexa(&args[i][1]), env->fd, pds[i]);
+				if (pds[i] == 2)
+				{
+					tmp = ft_atoi(&args[i][1]);
+					tmp < 0 ? tmp = (USHRT_MAX + 1) + tmp : 0;
+				}
+				else if (pds[i] == 4)
+				{
+					tmp = ft_atoi_l(&args[i][1]);
+					if (tmp < 0)
+						tmp = (UINT_MAX + 1) + (int)tmp;
+				}
+				ft_transform_size(tmp, env->fd_cor, pds[i]);
 			}
 		}
+		else if (args[i][0] && args[i][0] == ':')
+		{
+			ft_transform_size(ft_calcul_jump(env, &args[i][1], line, pds[i]), env->fd_cor, pds[i]);
+
+		}
 		else
-			ft_transform_size(ft_atoi_hexa(&args[i][1]), env->fd, pds[i]);
+		{
+			tmp = ft_atoi(&args[i][0]);
+			if (tmp < 0)
+			{
+				tmp = (USHRT_MAX + 1) + tmp;
+			}
+			ft_transform_size(tmp, env->fd_cor, pds[i]);
+		}
 		i++;
 	}
 }
@@ -103,28 +133,26 @@ static void		ft_write_opcode(t_asm *env, char *inst, int pds, char *op_next)
 
 	real_hex = ft_strnew_hex(1);
 	real_hex[0] += opcode;
-	// ft_printf("int %d\n", opcode);
-	write(env->fd, real_hex, 1);
+	write(env->fd_cor, real_hex, 1);
 	ft_strdel(&real_hex);
 
 	if (opcode != 1 && opcode != 9 && opcode != 12 && opcode != 15 && opcode != 16)
 	{
 		real_hex = ft_strnew_hex(1);
 		real_hex[0] += ft_atoi_base(op_next, 2);
-		// ft_printf("opnext [%s], int %d\n", op_next, ft_atoi_base(op_next, 2));
-		write(env->fd, real_hex, 1);
+		write(env->fd_cor, real_hex, 1);
 		ft_strdel(&real_hex);
 	}
 }
 
-static void		ft_write_empty(t_asm *env, int *i)
+void			ft_write_empty(t_asm *env, int *i)
 {
 	char		*real_hex;
 
 	while (env->file_content[(*i)][0] == '.')
 		(*i)++;
 	real_hex = ft_strnew_hex(4);
-	write(env->fd, real_hex, 4);
+	write(env->fd_cor, real_hex, 4);
 	ft_strdel(&real_hex);
 }
 
@@ -146,11 +174,23 @@ void			ft_write_instructions(t_asm *env)
 		}
 		i++;
 	}
-
-	// real_hex = ft_transform_size_helper(opcode, env->arg_weight[i][0]);
-	// write(env->fd, real_hex, env->arg_weight[i][0]);
-
-	// ft_transform_size(opcode, env->fd);
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
