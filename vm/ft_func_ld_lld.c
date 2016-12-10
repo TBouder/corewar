cc61f3fb1aae258bc726c55a440a92ab56720722
Tbouder <tbouder@protonmail.com>
2016-12-10 21:47:49 +0100

    Add macros + complete LLDI

--- vm/ft_func_ld_lld.c
+++ vm/ft_func_ld_lld.c
@@ -6,23 +6,23 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 18:38:40 by tbouder           #+#    #+#             */
-/*   Updated: 2016/12/10 20:25:36 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/12/10 21:40:21 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-#define LLD_IND_1		pc + arg1 //PC sur l'instruction suivante
-#define LLD_IND_2		pc - 1 + arg1 //PC sur le dernier argument
-#define LLD_IND_3		arg1 //Position sans prendre en compte le PC
-#define LLD_IND_4		champ->pc + arg1 //Depart du premier arg
-#define LLD_IND_5		champ->pc - 1 + arg1 //Depart de l'instruction courante
+#define LLD_IND_1		(pc + arg1) //PC sur l'instruction suivante
+#define LLD_IND_2		(pc - 1 + arg1) //PC sur le dernier argument
+#define LLD_IND_3		(arg1) //Position sans prendre en compte le PC
+#define LLD_IND_4		(champ->pc + arg1) //Depart du premier arg
+#define LLD_IND_5		(champ->pc - 1 + arg1) //Depart de l'instruction courante
 
-#define LD_IND_1		pc + (arg1 % IDX_MOD) //PC sur l'instruction suivante
-#define LD_IND_2		pc - 1 + (arg1 % IDX_MOD) //PC sur le dernier argument
-#define LD_IND_3		(arg1 % IDX_MOD) //Position sans prendre en compte le PC
-#define LD_IND_4		champ->pc + (arg1 % IDX_MOD) //Depart du premier arg
-#define LD_IND_5		champ->pc - 1 + (arg1 % IDX_MOD) //Depart de l'instruction courante
+#define LD_IND_1		(pc + (arg1 % IDX_MOD)) //PC sur l'instruction suivante
+#define LD_IND_2		(pc - 1 + (arg1 % IDX_MOD)) //PC sur le dernier argument
+#define LD_IND_3		((arg1 % IDX_MOD)) //Position sans prendre en compte le PC
+#define LD_IND_4		(champ->pc + (arg1 % IDX_MOD)) //Depart du premier arg
+#define LD_IND_5		(champ->pc - 1 + (arg1 % IDX_MOD)) //Depart de l'instruction courante
 
 static int	ft_set_buffer(int nbr)
 {
@@ -60,10 +60,10 @@ void		ft_corewar_lld(t_vm *env, t_champions *champ, int *nbr)
 		}
 		else if (IS_IND(nbr[0]))
 		{
-			champ->reg[arg2] = env->map[LLD_IND_5];
-			ft_put("{10}r%d{0} = env->map[{10}%d{0}]\n", arg2, LLD_IND_5);
+			champ->reg[arg2] = env->map[LLD_IND_5 % MEM_SIZE];
+			ft_put("{10}r%d{0} = env->map[{10}%d{0}]\n", arg2, LLD_IND_5 % MEM_SIZE);
 			ft_put("{10}r%d{0} = [{10}%c{0}] ([{10}0x%x{0}])\n",
-			arg2, env->map[LLD_IND_5], env->map[LLD_IND_5]);
+			arg2, env->map[LLD_IND_5 % MEM_SIZE], env->map[LLD_IND_5 % MEM_SIZE]);
 		}
 		champ->carry = champ->reg[arg2] == 0 ? 1 : 0;
 	}
@@ -94,12 +94,10 @@ void		ft_corewar_ld(t_vm *env, t_champions *champ, int *nbr)
 		}
 		else if (IS_IND(nbr[0]))
 		{
-			champ->reg[arg2] = env->map[LD_IND_5];
-			ft_put("{10}r%d{0} = env->map[{10}%d{0}]\n", arg2, LD_IND_5);
-			ft_put("{10}r%d{0} = [{10}%c{0}] ([{10}0x%x{0}])\n", arg2, env->map[LD_IND_5], env->map[LD_IND_5]);
+			champ->reg[arg2] = env->map[LD_IND_5 % MEM_SIZE];
+			ft_put("{10}r%d{0} = env->map[{10}%d{0}]\n", arg2, LD_IND_5 % MEM_SIZE);
+			ft_put("{10}r%d{0} = [{10}%c{0}] ([{10}0x%x{0}])\n", arg2, env->map[LD_IND_5 % MEM_SIZE], env->map[LD_IND_5 % MEM_SIZE]);
 		}
-			// OR ???
-			// champ->reg[arg2] += env->map[champ->pc + ((buffer + arg1) % IDX_MOD)];
 		champ->carry = champ->reg[arg2] == 0 ? 1 : 0;
 	}
 }
--- vm/ft_func_ldi_lldi.c
+++ vm/ft_func_ldi_lldi.c
@@ -6,29 +6,37 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 18:38:40 by tbouder           #+#    #+#             */
-/*   Updated: 2016/12/09 15:23:07 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/12/10 21:47:21 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-#define LDI_IND_1		pc + (sum_idx % IDX_MOD) //PC sur l'instruction suivante
-#define LDI_IND_2		pc - 1 + (sum_idx % IDX_MOD) //PC sur le dernier argument
-#define LDI_IND_3		(sum_idx % IDX_MOD) //Position sans prendre en compte le PC
-#define LDI_IND_4		champ->pc + (sum_idx % IDX_MOD) //Depart du premier arg
-#define LDI_IND_5		(champ->pc - 1 + sum_idx) % IDX_MOD //Depart de l'instruction courante
+#define LDI_IND_1		(pc + (sum_idx % IDX_MOD)) //PC sur l'instruction suivante
+#define LDI_IND_2		(pc - 1 + (sum_idx % IDX_MOD)) //PC sur le dernier argument
+#define LDI_IND_3		((sum_idx % IDX_MOD)) //Position sans prendre en compte le PC
+#define LDI_IND_4		(champ->pc + (sum_idx % IDX_MOD)) //Depart du premier arg
+#define LDI_IND_5		((champ->pc - 1 + sum_idx) % IDX_MOD) //Depart de l'instruction courante
 
-#define LDI_ARG_1		pc + (arg1 % IDX_MOD) //PC sur l'instruction suivante
-#define LDI_ARG_2		pc - 1 + (arg1 % IDX_MOD) //PC sur le dernier argument
-#define LDI_ARG_3		(arg1 % IDX_MOD) //Position sans prendre en compte le PC
-#define LDI_ARG_4		champ->pc + (arg1 % IDX_MOD) //Depart du premier arg
-#define LDI_ARG_5		champ->pc - 1 + (arg1 % IDX_MOD) //Depart de l'instruction courante
+#define LDI_ARG_1		(pc + (arg1 % IDX_MOD)) //PC sur l'instruction suivante
+#define LDI_ARG_2		(pc - 1 + (arg1 % IDX_MOD)) //PC sur le dernier argument
+#define LDI_ARG_3		((arg1 % IDX_MOD)) //Position sans prendre en compte le PC
+#define LDI_ARG_4		(champ->pc + (arg1 % IDX_MOD)) //Depart du premier arg
+#define LDI_ARG_5		(champ->pc - 1 + (arg1 % IDX_MOD)) //Depart de l'instruction courante
 
-#define LD_IND_1		pc + 1 + (arg1 % IDX_MOD) //PC sur l'instruction suivante
-#define LD_IND_2		pc + (arg1 % IDX_MOD) //PC sur le dernier argument
-#define LD_IND_3		(arg1 % IDX_MOD) //Position sans prendre en compte le PC
-#define LD_IND_4		champ->pc + (arg1 % IDX_MOD) //Depart du premier arg
-#define LD_IND_5		champ->pc - 1 + (arg1 % IDX_MOD) //Depart de l'instruction courante
+
+#define LLDI_IND_1		(pc + sum_idx) //PC sur l'instruction suivante
+#define LLDI_IND_2		(pc - 1 + sum_idx) //PC sur le dernier argument
+#define LLDI_IND_3		(sum_idx) //Position sans prendre en compte le PC
+#define LLDI_IND_4		(champ->pc + sum_idx) //Depart du premier arg
+#define LLDI_IND_5		(champ->pc - 1 + sum_idx) //Depart de l'instruction courante
+
+#define LLDI_ARG_1		(pc + arg1) //PC sur l'instruction suivante
+#define LLDI_ARG_2		(pc - 1 + arg1) //PC sur le dernier argument
+#define LLDI_ARG_3		(arg1) //Position sans prendre en compte le PC
+#define LLDI_ARG_4		(champ->pc + arg1) //Depart du premier arg
+#define LLDI_ARG_5		(champ->pc - 1 + arg1) //Depart de l'instruction courante
+
 
 static int	ft_set_buffer(int nbr)
 {
@@ -65,7 +73,7 @@ void		ft_corewar_ldi(t_vm *env, t_champions *champ, int *nbr)
 		pc++;
 
 		if (IS_IND(nbr[0]))
-			arg1 = env->map[LDI_ARG_5];
+			arg1 = env->map[LDI_ARG_5 % MEM_SIZE];
 		if (IS_REG(nbr[0]))
 			arg1 = champ->reg[arg1];
 		if (IS_REG(nbr[1]))
@@ -90,27 +98,71 @@ void		ft_corewar_ldi(t_vm *env, t_champions *champ, int *nbr)
 		ft_put("\033[104mchamp->reg[r%d] = env->map[%d]{0} || ", arg3, (champ->pc - 1 + sum_idx) % IDX_MOD);
 		ft_put("\033[104mchamp->reg[r%d] = env->map[0x%x]{0}\n", arg3, (champ->pc - 1 + sum_idx) % IDX_MOD);
 
-		ft_put("\033[104mchamp->reg[r%d] = %d{0} || ", arg3, env->map[(champ->pc - 1 + sum_idx) % IDX_MOD]);
-		ft_put("\033[104mchamp->reg[r%d] = %x{0}\n", arg3, env->map[champ->pc - 1 + sum_idx]);
+		ft_put("\033[104mchamp->reg[r%d] = %d{0} || ", arg3, env->map[((champ->pc - 1 + sum_idx) % IDX_MOD) % MEM_SIZE]);
+		ft_put("\033[104mchamp->reg[r%d] = %x{0}\n", arg3, env->map[(champ->pc - 1 + sum_idx) % MEM_SIZE]);
 
 		ft_print_memory(env->map, 160);
 
-		champ->reg[arg3] = ft_byte_to_str(&env->map[LDI_IND_5], 1);
+		champ->reg[arg3] = ft_byte_to_str(&env->map[LDI_IND_5 % MEM_SIZE], 1);
 		ft_put("\033[104mr%d = 0x%x{0}\n", arg3, champ->reg[arg3]);
-
-		champ->carry = champ->reg[arg2] == 0 ? 1 : 0;
 	}
-
-
 }
 
-void	ft_corewar_lldi(t_vm *env, t_champions *champ, int *nbr)
+void		ft_corewar_lldi(t_vm *env, t_champions *champ, int *nbr)
 {
-	if (0)
-	{
-		env = 0;
-		champ = 0;
-		nbr = 0;
-	}
 	ft_put("{9}----LLDI----{0}\n");
+	int		pc;
+	int		buffer;
+	int		arg1;
+	int		arg2;
+	int		arg3;
+	int		sum_idx;
+
+	// ft_put(champ->pc);
+	pc = champ->pc + 1;
+	if (IS_ALL(nbr[0]) && IS_DIR_REG(nbr[1]) && IS_REG(nbr[2]))
+	{
+		buffer = ft_set_buffer(nbr[0]);
+		arg1 = ft_byte_to_str(&env->map[pc], buffer);
+		pc += buffer;
+		buffer = ft_set_buffer(nbr[1]);
+		arg2 = ft_byte_to_str(&env->map[pc], buffer);
+		pc += buffer;
+		arg3 = ft_byte_to_str(&env->map[pc], 1);
+		pc++;
+
+		if (IS_IND(nbr[0]))
+			arg1 = env->map[LLDI_ARG_5 % MEM_SIZE];
+		if (IS_REG(nbr[0]))
+			arg1 = champ->reg[arg1];
+		if (IS_REG(nbr[1]))
+			arg2 = champ->reg[arg2];
+
+		sum_idx = arg1 + arg2; //SERA UNE ADRESSE DANS LAQUELLE ON VA LIRE UNE VALEUR DE LA TAILLE D'UN REGISTRE QU'ON MET DANS REG[arg3]
+
+		ft_put("{9}%d{0}\n", env->map[champ->pc]);
+
+		ft_put("\033[104m%d + %d = %d{0} || ", arg1, arg2, sum_idx);
+		ft_put("\033[104m0x%x + 0x%x = 0x%x{0}\n", arg1, arg2, sum_idx);
+
+		ft_put("\033[104mchamp->reg[r%d] = env->map[%d + %d + %d]{0} || ", arg3, champ->pc - 1, arg1, arg2);
+		ft_put("\033[104mchamp->reg[r%d] = env->map[0x%x + 0x%x + 0x%x]{0}\n", arg3, champ->pc - 1, arg1, arg2);
+
+		ft_put("\033[104mchamp->reg[r%d] = env->map[%d + %d]{0} || ", arg3, champ->pc - 1, sum_idx);
+		ft_put("\033[104mchamp->reg[r%d] = env->map[0x%x + 0x%x]{0}\n", arg3, champ->pc - 1, sum_idx);
+
+		ft_put("\033[104mchamp->reg[r%d] = env->map[%d]{0} || ", arg3, champ->pc - 1 + sum_idx);
+		ft_put("\033[104mchamp->reg[r%d] = env->map[0x%x]{0}\n", arg3, champ->pc - 1 + sum_idx);
+
+		ft_put("\033[104mchamp->reg[r%d] = env->map[%d]{0} || ", arg3, (champ->pc - 1 + sum_idx) % IDX_MOD);
+		ft_put("\033[104mchamp->reg[r%d] = env->map[0x%x]{0}\n", arg3, (champ->pc - 1 + sum_idx) % IDX_MOD);
+
+		ft_put("\033[104mchamp->reg[r%d] = %d{0} || ", arg3, env->map[((champ->pc - 1 + sum_idx) % IDX_MOD) % MEM_SIZE]);
+		ft_put("\033[104mchamp->reg[r%d] = %x{0}\n", arg3, env->map[(champ->pc - 1 + sum_idx) % MEM_SIZE]);
+
+		ft_print_memory(env->map, 160);
+
+		champ->reg[arg3] = ft_byte_to_str(&env->map[LLDI_IND_5 % MEM_SIZE], 1);
+		ft_put("\033[104mr%d = 0x%x{0}\n", arg3, champ->reg[arg3]);
+	}
 }
