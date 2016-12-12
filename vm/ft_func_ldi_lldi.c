ca4a86fff26168979d0bb395bd7440bd74eee153
Tbouder <tbouder@protonmail.com>
2016-12-12 14:31:09 +0100

    LDI LLDI

--- vm/ft_func_ldi_lldi.c
+++ vm/ft_func_ldi_lldi.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 18:38:40 by tbouder           #+#    #+#             */
-/*   Updated: 2016/12/10 21:47:21 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/12/12 14:30:56 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,25 @@
 #define LDI_IND_2		(pc - 1 + (sum_idx % IDX_MOD)) //PC sur le dernier argument
 #define LDI_IND_3		((sum_idx % IDX_MOD)) //Position sans prendre en compte le PC
 #define LDI_IND_4		(champ->pc + (sum_idx % IDX_MOD)) //Depart du premier arg
-#define LDI_IND_5		((champ->pc - 1 + sum_idx) % IDX_MOD) //Depart de l'instruction courante
+#define LDI_IND_5		((champ->pc - 1 + (sum_idx % IDX_MOD)) % MEM_SIZE) //Depart de l'instruction courante
 
 #define LDI_ARG_1		(pc + (arg1 % IDX_MOD)) //PC sur l'instruction suivante
 #define LDI_ARG_2		(pc - 1 + (arg1 % IDX_MOD)) //PC sur le dernier argument
 #define LDI_ARG_3		((arg1 % IDX_MOD)) //Position sans prendre en compte le PC
 #define LDI_ARG_4		(champ->pc + (arg1 % IDX_MOD)) //Depart du premier arg
-#define LDI_ARG_5		(champ->pc - 1 + (arg1 % IDX_MOD)) //Depart de l'instruction courante
-
+#define LDI_ARG_5		((champ->pc - 1 + (arg1 % IDX_MOD)) % MEM_SIZE) //Depart de l'instruction courante
 
 #define LLDI_IND_1		(pc + sum_idx) //PC sur l'instruction suivante
 #define LLDI_IND_2		(pc - 1 + sum_idx) //PC sur le dernier argument
 #define LLDI_IND_3		(sum_idx) //Position sans prendre en compte le PC
 #define LLDI_IND_4		(champ->pc + sum_idx) //Depart du premier arg
-#define LLDI_IND_5		(champ->pc - 1 + sum_idx) //Depart de l'instruction courante
+#define LLDI_IND_5		((champ->pc - 1 + sum_idx) % MEM_SIZE) //Depart de l'instruction courante
 
 #define LLDI_ARG_1		(pc + arg1) //PC sur l'instruction suivante
 #define LLDI_ARG_2		(pc - 1 + arg1) //PC sur le dernier argument
 #define LLDI_ARG_3		(arg1) //Position sans prendre en compte le PC
 #define LLDI_ARG_4		(champ->pc + arg1) //Depart du premier arg
-#define LLDI_ARG_5		(champ->pc - 1 + arg1) //Depart de l'instruction courante
-
+#define LLDI_ARG_5		((champ->pc - 1 + arg1) % MEM_SIZE) //Depart de l'instruction courante
 
 static int	ft_set_buffer(int nbr)
 {
@@ -72,16 +70,16 @@ void		ft_corewar_ldi(t_vm *env, t_champions *champ, int *nbr)
 		arg3 = ft_byte_to_str(&env->map[pc], 1);
 		pc++;
 
+		ft_put("{9}%d\n{0}", LDI_ARG_5); //POSITION DE L'ADRESSE
+
 		if (IS_IND(nbr[0]))
-			arg1 = env->map[LDI_ARG_5 % MEM_SIZE];
+			arg1 = env->map[LDI_ARG_5];
 		if (IS_REG(nbr[0]))
 			arg1 = champ->reg[arg1];
 		if (IS_REG(nbr[1]))
 			arg2 = champ->reg[arg2];
 
 		sum_idx = arg1 + arg2; //SERA UNE ADRESSE DANS LAQUELLE ON VA LIRE UNE VALEUR DE LA TAILLE D'UN REGISTRE QU'ON MET DANS REG[arg3]
-
-		ft_put("{9}%d{0}\n", env->map[champ->pc]);
 
 		ft_put("\033[104m%d + %d = %d{0} || ", arg1, arg2, sum_idx);
 		ft_put("\033[104m0x%x + 0x%x = 0x%x{0}\n", arg1, arg2, sum_idx);
@@ -101,9 +99,9 @@ void		ft_corewar_ldi(t_vm *env, t_champions *champ, int *nbr)
 		ft_put("\033[104mchamp->reg[r%d] = %d{0} || ", arg3, env->map[((champ->pc - 1 + sum_idx) % IDX_MOD) % MEM_SIZE]);
 		ft_put("\033[104mchamp->reg[r%d] = %x{0}\n", arg3, env->map[(champ->pc - 1 + sum_idx) % MEM_SIZE]);
 
-		ft_print_memory(env->map, 160);
+		ft_print_memory(env->map, 80);
 
-		champ->reg[arg3] = ft_byte_to_str(&env->map[LDI_IND_5 % MEM_SIZE], 1);
+		champ->reg[arg3] = ft_byte_to_str(&env->map[LDI_IND_5], 1);
 		ft_put("\033[104mr%d = 0x%x{0}\n", arg3, champ->reg[arg3]);
 	}
 }
@@ -138,7 +136,7 @@ void		ft_corewar_lldi(t_vm *env, t_champions *champ, int *nbr)
 		if (IS_REG(nbr[1]))
 			arg2 = champ->reg[arg2];
 
-		sum_idx = arg1 + arg2; //SERA UNE ADRESSE DANS LAQUELLE ON VA LIRE UNE VALEUR DE LA TAILLE D'UN REGISTRE QU'ON MET DANS REG[arg3]
+		sum_idx = arg1 + arg2;
 
 		ft_put("{9}%d{0}\n", env->map[champ->pc]);
 
@@ -154,15 +152,16 @@ void		ft_corewar_lldi(t_vm *env, t_champions *champ, int *nbr)
 		ft_put("\033[104mchamp->reg[r%d] = env->map[%d]{0} || ", arg3, champ->pc - 1 + sum_idx);
 		ft_put("\033[104mchamp->reg[r%d] = env->map[0x%x]{0}\n", arg3, champ->pc - 1 + sum_idx);
 
-		ft_put("\033[104mchamp->reg[r%d] = env->map[%d]{0} || ", arg3, (champ->pc - 1 + sum_idx) % IDX_MOD);
-		ft_put("\033[104mchamp->reg[r%d] = env->map[0x%x]{0}\n", arg3, (champ->pc - 1 + sum_idx) % IDX_MOD);
+		ft_put("\033[104mchamp->reg[r%d] = env->map[%d]{0} || ", arg3, (champ->pc - 1 + sum_idx));
+		ft_put("\033[104mchamp->reg[r%d] = env->map[0x%x]{0}\n", arg3, (champ->pc - 1 + sum_idx));
 
-		ft_put("\033[104mchamp->reg[r%d] = %d{0} || ", arg3, env->map[((champ->pc - 1 + sum_idx) % IDX_MOD) % MEM_SIZE]);
+		ft_put("\033[104mchamp->reg[r%d] = %d{0} || ", arg3, env->map[((champ->pc - 1 + sum_idx)) % MEM_SIZE]);
 		ft_put("\033[104mchamp->reg[r%d] = %x{0}\n", arg3, env->map[(champ->pc - 1 + sum_idx) % MEM_SIZE]);
 
-		ft_print_memory(env->map, 160);
+		ft_print_memory(env->map, 80);
 
-		champ->reg[arg3] = ft_byte_to_str(&env->map[LLDI_IND_5 % MEM_SIZE], 1);
+		champ->reg[arg3] = ft_byte_to_str(&env->map[LLDI_IND_5], 1);
+		//CARRY
 		ft_put("\033[104mr%d = 0x%x{0}\n", arg3, champ->reg[arg3]);
 	}
 }
--- vm/test.s
+++ vm/test.s
@@ -2,7 +2,7 @@
 .name    "Survivor"
 .comment "Invincible"
 
-################################################################################
+#LD#############################################################################
 	; ; TEST DE LD DIRECT ET INDIRECT
 	; ################################
 	; ld %68, r2			#Met %68 dans r2 -> 'D'
@@ -11,7 +11,6 @@
 	; aff r7				#Affiche r7 -> ''
 	; add r2, r7, r3		#r3 = r2 + r7 -> 0x4 + 0x44 -> 'H'
 	; aff r3				#Affiche r3 -> 'H'
-
 	; ; TEST DE LLD DIRECT ET INDIRECT
 	; ################################
 	; lld %68, r2			#Met %68 dans r2 -> D
@@ -21,7 +20,7 @@
 	; sub r2, r7, r3		#r2 = r7 - r3 -> 0x5 + 0x44 -> '?'
 	; aff r3				#Affiche r3 -> '?'
 
-################################################################################
+#ST#############################################################################
 	; ; TEST DE ST REG
 	; ################################
 	; ld %68, r2			#Met %68 dans r2 -> 'D'
@@ -30,7 +29,6 @@
 	; aff r2				#Affiche r2 -> 'D'
 	; st r2, r3			#Stock r2 dans r3
 	; aff r3				#Affiche r3 -> 'D'
-
 	; ; TEST DE ST IND
 	; ################################
 	; ld %68, r2			#Met %68 dans r2 -> 'D'
@@ -41,7 +39,7 @@
 	; ld 37, r3			#Met ce qu'il y a dans 37 dans r3 -> 'D'
 	; aff r3				#Affiche r3 -> 'D'
 
-################################################################################
+#STI############################################################################
 	; ; TEST DE STI DIRECT-DIRECT
 	; ################################
 	; ld %68, r2			#Met %68 dans r2
@@ -51,7 +49,6 @@
 	; sti r2, %29, %40	#Stock r2 a l'adresse de PC + ((%29 + %40) % IDX_MOD)
 	; ld 62, r7			#Stock dans r7 ce qu'il y a a l'adresse PC + 62
 	; aff r7				#Affiche r7 -> 'D'
-
 	; ; TEST DE STI DIRECT-REG
 	; ################################
 	; ld %68, r2			#Met %68 dans r2
@@ -61,8 +58,6 @@
 	; sti r2, %29, r2		#Stock r2 a l'adresse de PC + ((%29 + r2[VAL]) % IDX_MOD)
 	; ld 91, r7			#Stock dans r7 ce qu'il y a a l'adresse PC + 91
 	; aff r7				#Affiche r7 -> 'D'
-
-
 	; ; TEST DE STI REG-REG
 	; ################################
 	; ld %68, r2			#Met %68 dans r2
@@ -72,7 +67,6 @@
 	; sti r2, r2, r2		#Stock r2 a l'adresse de PC + ((r2[VAL] + r2[VAL]) % IDX_MOD)
 	; ld 131, r7			#Stock dans r7 ce qu'il y a a l'adresse PC + 131
 	; aff r7				#Affiche r7 -> 'D'
-
 	; ; TEST DE STI IND-REG
 	; ################################
 	; ld %68, r2			#Met %68 dans r2
@@ -82,7 +76,6 @@
 	; sti r2, 11, r2		#Stock r2 a l'adresse de PC + ((r2[VAL] + (PC + 11)) % IDX_MOD)
 	; ld 91, r7			#Stock dans r7 ce qu'il y a a l'adresse PC + 91
 	; aff r7				#Affiche r7 -> 'D'
-
 	; ; TEST DE STI IND-REG
 	; ################################
 	; ld %68, r2			#Met %68 dans r2
@@ -92,17 +85,95 @@
 	; sti r2, 11, %29		#Stock r2 a l'adresse de PC + (((PC + 11) + %29) % IDX_MOD)
 	; ld 51, r7			#Stock dans r7 ce qu'il y a a l'adresse PC + 51
 	; aff r7				#Affiche r7 -> 'D'
-################################################################################
 
+#LDI############################################################################
 	; ; TEST DE LDI REG-REG
-	; ################################
-	ld %68, r2			#Met %68 dans r2
-	add r2, r1, r1		#r1 (le dernier) = r2 + r1
-	aff r1				#Affiche r1
-	aff r2				#Affiche r2
-	ldi %-10, %0, r7		#Stock r2 a l'adresse de PC + ((%29 + %68) % IDX_MOD)
-	aff r7				#Affiche r7 -> ''
-	; ldi %1, %0, r7		#Stocker dans r7 ce qu'il y a a l'adresse de %1 + %1
+	; ld %-11, r2			#Met %-11 dans r2
+	; add r2, r1, r3		#r3 = r2 + r1
+	; aff r3				#Affiche r3 -> ''
+	; aff r2				#Affiche r2 -> ''
+	; ldi r2, r1, r7		# additionne r2 et r1, et va mettre la valeur a l'adresse de [r2 + r1] dans r7
+	; aff r7				#Affiche r7 -> 'T'
+	; ; TEST DE LDI REG-DIR
+	; ld %68, r2			#Met %68 dans r2
+	; add r2, r1, r3		#r3 = r2 + r1
+	; aff r3				#Affiche r3 -> 'E'
+	; aff r2				#Affiche r2 -> 'D'
+	; ldi r1, %0, r7	# additionne %0 et r1, et va mettre la valeur a l'adresse de [%0 + r1] dans r7
+	; aff r7				#Affiche r7 -> 'd'
+	; ; TEST DE LDI DIR-DIR
+	; ld %68, r2			#Met %68 dans r2
+	; add r2, r1, r1		#r1 (le dernier) = r2 + r1
+	; aff r1				#Affiche r1 -> 'E'
+	; aff r2				#Affiche r2 -> 'D'
+	; ldi %1, %0, r7	# additionne %1 et %0, et va mettre la valeur a l'adresse de [%1 + %0] dans r7
+	; aff r7				#Affiche r7 -> '¤'
+	; ; TEST DE LDI DIR-REG
+	; ld %68, r2			#Met %68 dans r2
+	; add r2, r1, r3		#r3 = r2 + r1
+	; aff r3				#Affiche r3 -> 'E'
+	; aff r2				#Affiche r2 -> 'D'
+	; ldi %0, r8, r7	# additionne %0 et r8, et va mettre la valeur a l'adresse de [%0 + r8] dans r7
+	; aff r7				#Affiche r7 -> '\n'
+	; ; TEST DE LDI IND-DIR
+	; ld %68, r2			#Met %68 dans r2
+	; add r2, r1, r3		#r3 = r2 + r1
+	; aff r3				#Affiche r3 -> 'E'
+	; aff r2				#Affiche r2 -> 'D'
+	; ldi 2, %1, r7		#On va chercher ce qu'il y a a l'adresse de (PC + 2 -> 18 + 2), on prend la valeur, on additionne avec la valeur dans R1 [map[pc + 2] + r1] et on met dans r7
+	; aff r7				#Affiche r7 -> 'ä'
+	; ; TEST DE LDI IND-REG
+	; ld %68, r2			#Met %68 dans r2
+	; add r2, r1, r3		#r3 = r2 + r1
+	; aff r3				#Affiche r3 -> 'E'
+	; aff r2				#Affiche r2 -> 'D'
+	; ldi 2, r1, r7		#On va chercher ce qu'il y a a l'adresse de (PC + 2 -> 18 + 2), on prend la valeur, on additionne avec la valeur dans R1 [map[pc + 2] + r1] et on met dans r7
+	; aff r7				#Affiche r7 -> 'Ô'
+
+#LLDI###########################################################################
+	; ; TEST DE LDI REG-REG
+	; ld %-11, r2			#Met %-11 dans r2
+	; add r2, r1, r3		#r3 = r2 + r1
+	; aff r3				#Affiche r3 -> ''
+	; aff r2				#Affiche r2 -> ''
+	; lldi r2, r1, r7		# additionne r2 et r1, et va mettre la valeur a l'adresse de [r2 + r1] dans r7
+	; aff r7				#Affiche r7 -> 'T'
+	; ; TEST DE LDI REG-DIR
+	; ld %68, r2			#Met %68 dans r2
+	; add r2, r1, r3		#r3 = r2 + r1
+	; aff r3				#Affiche r3 -> 'E'
+	; aff r2				#Affiche r2 -> 'D'
+	; lldi r1, %-11, r7	# additionne %-11 et r1, et va mettre la valeur a l'adresse de [%-10 + r1] dans r7
+	; aff r7				#Affiche r7 -> 'T'
+	; ; TEST DE LDI DIR-DIR
+	; ld %68, r2			#Met %68 dans r2
+	; add r2, r1, r1		#r1 (le dernier) = r2 + r1
+	; aff r1				#Affiche r1 -> 'E'
+	; aff r2				#Affiche r2 -> 'D'
+	; lldi %-10, %0, r7	# additionne %-10 et %0, et va mettre la valeur a l'adresse de [%-10 + %0] dans r7
+	; aff r7				#Affiche r7 -> 'T'
+	; ; TEST DE LDI DIR-REG
+	; ld %68, r2			#Met %68 dans r2
+	; add r2, r1, r3		#r3 = r2 + r1
+	; aff r3				#Affiche r3 -> 'E'
+	; aff r2				#Affiche r2 -> 'D'
+	; lldi %-11, r1, r7	# additionne %-11 et r1, et va mettre la valeur a l'adresse de [%-10 + r1] dans r7
+	; aff r7				#Affiche r7 -> 'T'
+	; ; TEST DE LDI IND-DIR
+	; ld %68, r2			#Met %68 dans r2
+	; add r2, r1, r3		#r3 = r2 + r1
+	; aff r3				#Affiche r3 -> 'E'
+	; aff r2				#Affiche r2 -> 'D'
+	; lldi -7, %0, r7		#On va chercher ce qu'il y a a l'adresse de (PC - 7 -> 18 + 03), on prend la valeur, on additionne avec la valeur %0 [map[pc + 03] + 0] et on met dans r7
+	; aff r7				#Affiche r7 -> 'ù'
+	; ; TEST DE LDI IND-REG
+	; ld %68, r2			#Met %68 dans r2
+	; add r2, r1, r3		#r3 = r2 + r1
+	; aff r3				#Affiche r3 -> 'E'
+	; aff r2				#Affiche r2 -> 'D'
+	; lldi -8, r1, r7		#On va chercher ce qu'il y a a l'adresse de (PC - 8 -> 18 + 01), on prend la valeur, on additionne avec la valeur dans R1 [map[pc + 01] + R1] et on met dans r7
+	; aff r7				#Affiche r7 -> 'ÿ'
+
 
 	################################################################################
 	################################################################################
